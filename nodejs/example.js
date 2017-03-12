b766bba8071d51ecf918f68c6026f981a0bc15c5
jslinuxboy <2069284231@qq.com>
2017-03-12 22:42:47 +0800

    version 1.0.1

--- nodejs/example.js
+++
@@ -1,7 +0,0 @@
-var ID3fs = require('fs');
-var ID3 = require("./ID3");
-
-ID3fs.readFile("xxxx.mp3", function(err, data){
-	var d = ID3.ID3fileReader(data, ["title", "picture"]);
-	console.log(d);
-});
